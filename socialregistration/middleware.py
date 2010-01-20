c51e7236f0f85b58edb8c7864eadae1f4e62cc8a
Alen Mujezinovic <flashingpumpkin@gmail.com>
2010-01-20 16:07:59 +0000

    Docstring on middleware

--- socialregistration/middleware.py
+++ socialregistration/middleware.py
@@ -2,7 +2,13 @@ from django.utils.translation import gettext as _
 
 class FacebookMiddleware(object):
     def process_request(self, request):
-        if getattr(request, 'facebook', None):
+        """
+        Enables request.facebook in your views once the user authenticated the
+        application and connected with facebook. You might want to use this
+        if you don't feel confortable with the javascript library.
+        """
+        if getattr(request, 'facebook', None) is None:
             raise AttributeError(_('Please make sure you have *facebook.djangofb.FacebookMiddleware* installed as a middleware.'))
         else:
             request.facebook.check_session(request)
+        return None

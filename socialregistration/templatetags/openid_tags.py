94db7fb72741bde73b21d82250d518eff621c8ea
Tom Drummond <tom@devioustree.co.uk>
2010-02-09 15:10:51 +0000

    Added logic to openid_form

--- socialregistration/templatetags/openid_tags.py
+++ socialregistration/templatetags/openid_tags.py
@@ -1,12 +1,16 @@
 """
 Created on 24.09.2009
 
-@author: alen
+@author: alen, tom
 """
 from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('socialregistration/openid_form.html')
-def openid_form():
-    return {}
+@register.inclusion_tag('socialregistration/openid_form.html', takes_context=True)
+def openid_form(context):
+    if not 'request' in context:
+        raise AttributeError, 'Please add the ``django.core.context_processors.request`` context processors to your settings.CONTEXT_PROCESSORS set'
+    logged_in = context['request'].user.is_authenticated()
+    next = context['next'] if 'next' in context else None
+    return dict(next=next, logged_in=logged_in)

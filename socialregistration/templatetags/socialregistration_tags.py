e0aab62f2a693ca20a81c9e55c4220f379ac9eb1
Adam Fast <adamfast@georgie.servers.ljworld.com>
2010-04-28 12:14:49 -0500

    Add necessary arguments to the social_csrf_token tag.

--- socialregistration/templatetags/socialregistration_tags.py
+++ socialregistration/templatetags/socialregistration_tags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.tag
-def social_csrf_token():
+def social_csrf_token(parser, token):
     """
     Wrapper around the ``{% csrf_token %}`` template tag to make socialregistration
     work with both Django v1.2 and Django < v1.2

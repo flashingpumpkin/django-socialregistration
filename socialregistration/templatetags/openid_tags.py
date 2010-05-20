370981b09bccda621bc81639208ba2b9413ba4ad
Adam Fast <adam@adamfast.com>
2010-05-19 23:25:49 -0500

    Removed non-2.4 compatible syntax from openid/twitter templatetags as well.

--- socialregistration/templatetags/openid_tags.py
+++ socialregistration/templatetags/openid_tags.py
@@ -7,5 +7,8 @@ def openid_form(context):
     if not 'request' in context:
         raise AttributeError, 'Please add the ``django.core.context_processors.request`` context processors to your settings.TEMPLATE_CONTEXT_PROCESSORS set'
     logged_in = context['request'].user.is_authenticated()
-    next = context['next'] if 'next' in context else None
+    if 'next' in context:
+        next = context['next']
+    else:
+        next = None
     return dict(next=next, logged_in=logged_in)

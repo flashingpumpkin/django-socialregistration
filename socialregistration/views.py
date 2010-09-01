37a6aa2095d4ca2076aa90ab183237c5f4bdcbc5
Adrián Ribao Martínez <adrian@adrima.es>
2010-09-01 16:29:26 +0200

    Check if the session key exists in request.session before deleting it.

--- socialregistration/views.py
+++ socialregistration/views.py
@@ -66,8 +66,8 @@ def setup(request, template='socialregistration/setup.html',
                 user = form.profile.authenticate()
                 login(request, user)
 
-                del request.session['socialregistration_user']
-                del request.session['socialregistration_profile']
+                if 'socialregistration_user' in request.session: del request.session['socialregistration_user']
+                if 'socialregistration_profile' in request.session: del request.session['socialregistration_profile']
 
                 return HttpResponseRedirect(_get_next(request))
 
@@ -89,8 +89,8 @@ def setup(request, template='socialregistration/setup.html',
         login(request, user)
 
         # Clear & Redirect
-        del request.session['socialregistration_user']
-        del request.session['socialregistration_profile']
+        if 'socialregistration_user' in request.session: del request.session['socialregistration_user']
+        if 'socialregistration_profile' in request.session: del request.session['socialregistration_profile']
         return HttpResponseRedirect(_get_next(request))
 
 if has_csrf:

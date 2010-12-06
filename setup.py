76a23c66776e4f7b4d9215a432f19d8b26b5b7e5
Alen Mujezinovic <flashingpumpkin@gmail.com>
2010-12-06 12:02:22 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 METADATA = dict(
     name='django-socialregistration',
-    version='0.4.2',
+    version='0.4.3',
     author='Alen Mujezinovic',
     author_email='alen@caffeinehit.com',
     description='Django application enabling registration through a variety of APIs',

2564c1c0a00d54ed41cc93684d8a9b8f098bf598
James Addison <code@scottisheyes.com>
2011-09-27 10:13:08 -0700

    Improving the reliability and simplicity of the middleware changes.

--- socialregistration/middleware.py
+++ socialregistration/middleware.py
@@ -4,60 +4,49 @@ from django.conf import settings
 
 class Facebook(object):
     def __init__(self, user=None):
-        if user is None:
-            self.uid = None
-        else:
-            self.uid = user['uid']
-            self.user = user
+        self.user = user
+        if user:
+            self.uid = user.get('user_id', '')
 
-        self.access_token = None
+        self.access_token = ''
 
     def get_graph(self):
-        if not self.access_token:
-            self.access_token = facebook.get_user_access_token(
-                self.user.get('code', ''),
-                getattr(
-                    settings,
-                    'FACEBOOK_APP_ID',
-                    settings.FACEBOOK_API_KEY
-                ),
-                settings.FACEBOOK_SECRET_KEY
-            ).get('access_token', None)
+        if self.user and not self.access_token:
+            user_code = self.user.get('code', '')
+            if user_code:
+                self.access_token = facebook.get_user_access_token(
+                    user_code,
+                    getattr(
+                        settings,
+                        'FACEBOOK_APP_ID',
+                        settings.FACEBOOK_API_KEY
+                    ),
+                    settings.FACEBOOK_SECRET_KEY
+                ).get('access_token', '')
 
-        return facebook.GraphAPI(self.access_token)
+        return self.access_token and facebook.GraphAPI(self.access_token) or None
 
 
 class FacebookMiddleware(object):
     def process_request(self, request):
         """
-        Enables ``request.facebook`` and ``request.facebook.graph`` in your views 
+        Enables ``request.facebook`` and ``request.facebook.get_graph`` in your views
         once the user authenticated the  application and connected with facebook. 
         You might want to use this if you don't feel confortable with the 
         javascript library.
         """
-        
-        data = facebook.get_user_from_cookie(request.COOKIES,
-            getattr(settings, 'FACEBOOK_APP_ID', settings.FACEBOOK_API_KEY), settings.FACEBOOK_SECRET_KEY)
-
-        if data is None:
-            # maybe OAuth 2
-            data = facebook.parse_signed_request(
-                request.COOKIES.get(
-                    'fbsr_' + getattr(
-                        settings,
-                        'FACEBOOK_APP_ID',
-                        settings.FACEBOOK_API_KEY
-                        ),
-                    '',
+        data = facebook.parse_signed_request(
+            request.COOKIES.get(
+                'fbsr_' + getattr(
+                    settings,
+                    'FACEBOOK_APP_ID',
+                    settings.FACEBOOK_API_KEY
                     ),
-                settings.FACEBOOK_SECRET_KEY,
-                )
-            if data:
-                data['uid'] = data['user_id']
-            else:
-                data = None
+                '',
+                ),
+            settings.FACEBOOK_SECRET_KEY,
+            ) or {}
 
-        if data is not None:
-            request.facebook = Facebook(data)
+        request.facebook = Facebook(data)
         
         return None

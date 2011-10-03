1dec6c54dc27c6f364aad12c5825db785adffa6f
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-10-03 19:41:02 +0100

    More backwards compatibility

---
+++ socialregistration/models.py
@@ -0,0 +1,8 @@
+from socialregistration.contrib.facebook.models import FacebookAccessToken, \
+    FacebookProfile
+from socialregistration.contrib.linkedin.models import LinkedInAccessToken, \
+    LinkedInProfile, LinkedInRequestToken
+from socialregistration.contrib.openid.models import OpenIDNonce, OpenIDProfile, \
+    OpenIDStore
+from socialregistration.contrib.twitter.models import TwitterAccessToken, \
+    TwitterProfile, TwitterRequestToken

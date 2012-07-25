33df8d60ee7a93a68293bfed038ee2d79fb61fba
Alen Mujezinovic <flashingpumpkin@gmail.com>
2012-07-25 18:02:33 +0100

    Added missing Instagram options.
    Works on live API now.

--- socialregistration/contrib/instagram/client.py
+++ socialregistration/contrib/instagram/client.py
@@ -9,7 +9,7 @@ import json
 class Instagram(OAuth2):
     client_id = getattr(settings, 'INSTAGRAM_CLIENT_ID', '')
     secret = getattr(settings, 'INSTAGRAM_CLIENT_SECRET', '')
-    scope = getattr(settings, 'INSTAGRAM_REQUEST_PERMISSIONS', '')
+    scope = getattr(settings, 'INSTAGRAM_REQUEST_PERMISSIONS', 'basic')
     
     auth_url = 'https://api.instagram.com/oauth/authorize/'
     access_token_url = 'https://api.instagram.com/oauth/access_token/'
@@ -22,6 +22,14 @@ class Instagram(OAuth2):
                 reverse('socialregistration:instagram:callback'))
         return 'http://%s%s' % (Site.objects.get_current().domain,
             reverse('socialregistration:instagram:callback'))
+
+    def get_access_token(self, **params):
+        """
+        Instagram requires a `grant_type` parameter when requesting the access
+        token. 
+        """
+        return super(Instagram, self).get_access_token(grant_type='authorization_code', **params)
+
 
     def parse_access_token(self, content):
         return json.loads(content)

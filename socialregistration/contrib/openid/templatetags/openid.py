781d19b5d1417fdb648b7b512341c4aac6f1bc75
Darie Petrov <darie@cambriansoft.org>
2012-10-17 19:28:51 -0400

    openid_form tag accepts an arbitrary number of context variables and strings that get concatenated to generate the button source url

--- socialregistration/contrib/openid/templatetags/openid.py
+++ socialregistration/contrib/openid/templatetags/openid.py
@@ -4,10 +4,11 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('socialregistration/openid/form.html', takes_context=True)
-def openid_form(context, provider=None, button=None):
+def openid_form(context, provider=None, *params):
+    button = ''.join(params)
     return {
         'provider': provider,
         'button': button,
         'request': context['request']
     }
-    
+

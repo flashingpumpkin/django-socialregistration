f7b9ac2fc6574c98fd81866f670860da8eeeca67
Jesper Noehr <jesper@noehr.org>
2012-12-28 11:40:58 -0800

    Made Google auth find existing profiles so we can log people in with it

--- socialregistration/contrib/google/auth.py
+++ socialregistration/contrib/google/auth.py
@@ -4,10 +4,14 @@ from django.contrib.auth.backends import ModelBackend
 
 
 class GoogleAuth(ModelBackend):
-    def authenticate(self, id = None):
+    supports_object_permissions = False
+    supports_anonymous_user = False
+
+    def authenticate(self, **kwargs):
+        uid = kwargs.get('google_id')
         try:
             return GoogleProfile.objects.get(
-                google_id = id,
+                google_id = uid,
                 site = Site.objects.get_current()).user
         except GoogleProfile.DoesNotExist:
             return None

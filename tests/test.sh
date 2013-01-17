38320bdee4067cdbfc34f52b76fe377f60829f86
Alen Mujezinovic <flashingpumpkin@gmail.com>
2013-01-17 10:31:32 +0000

    Tox / Travis adjustment

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export PYTHONPATH=$PWD/../:$PYTHONPATH
+PYTHONPATH=$PWD/../:$PYTHONPATH
 
 django-admin.py test facebook foursquare twitter github instagram linkedin openid tumblr twitter --settings tests.settings
